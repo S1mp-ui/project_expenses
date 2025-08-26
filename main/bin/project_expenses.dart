7fc5f3ad27e1d220c1a6cc741ecdfd12b247e592
Yutthawee <6631501097@lamduan.mfu.ac.th>
2025-08-27 00:49:18 +0700

    search

--- main/bin/project_expenses.dart
+++ main/bin/project_expenses.dart
@@ -114,8 +114,36 @@ void main() async {
     } 
     
     ////===Choice 3=== Search Expenses ====
-    else if (){
-      
+   else if (choice == "3") {
+      stdout.write('\nEnter search term: ');
+      String? search = stdin.readLineSync();
+      if (search == null || search.isEmpty) {
+        print('Invalid input.\n');
+        continue;
+      }
+
+      var res = await http.get(
+        Uri.parse('http://localhost:3000/expenses?user_id=$userId'),
+      );
+      List<dynamic> data = jsonDecode(res.body);
+
+      String searchLower = search.toLowerCase();
+
+      var results = data
+          .where(
+            (e) => e['item'].toString().toLowerCase().contains(searchLower),
+          )
+          .toList();
+
+      if (results.isEmpty) {
+        print('No matching expenses found.\n');
+      } else {
+        print('Search Results:');
+        for (var e in results) {
+          print('${e['item']}: ${e['paid']} ฿');
+        }
+        print('');
+      }
     }
    
 
